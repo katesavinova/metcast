a8fdeeed6fd504ce2d50972a54118f79973bd07e
katesavinova <63250525+katesavinova@users.noreply.github.com>
2020-06-24 18:29:22 +0400

    added form for choose city

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
+const API_KEY = "3c65d7ce8313b695684ad6a3de842b27";
+
+class App extends React.Component {
+  gettingWeater = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <Info />
+        <Form weatherMethod={this.gettingWeater} />
+        <Weather />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <form><input type="text"></input></form>
+            </div>
+        );
+    }
+}
+export default Weather;
